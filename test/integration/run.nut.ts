daf14e7b58d7fc33398b2cdaa17a687100a4601c
Sébastien Colladon <colladonsebastien@gmail.com>
2025-02-27 11:23:17 +0100

    fix: nuts tests

--- test/integration/run.nut.ts
+++ test/integration/run.nut.ts
@@ -1,33 +1,11 @@
-import { TestSession, execCmd } from '@salesforce/cli-plugins-testkit'
+import { execCmd } from '@salesforce/cli-plugins-testkit'
 import { expect } from 'chai'
-import { after, before, describe, it } from 'mocha'
+import { describe, it } from 'mocha'
 import { ApexMutationTestResult } from '../../src/commands/apex/mutation/test/run.js'
 
 //import { join } from 'node:path'
 
 describe('apex mutation test run NUTs', () => {
-  let testSession: TestSession
-
-  before(async () => {
-    testSession = await TestSession.create({
-      /*project: {
-        sourceDir: join('test', 'data', 'mutation-repo'),
-      },
-      devhubAuthStrategy: 'AUTO',
-      scratchOrgs: [
-        {
-          setDefault: true,
-          config: join('config', 'project-scratch-def.json'),
-          tracksSource: false,
-        },
-      ],*/
-    })
-  })
-
-  after(async () => {
-    await testSession?.clean()
-  })
-
   it('should display help', () => {
     // Act
     const result = execCmd<ApexMutationTestResult>(
@@ -41,45 +19,38 @@ describe('apex mutation test run NUTs', () => {
     expect(result).to.include('mutation')
   })
 
-  /* 
-  TODO create a dedicated org for this test
-  It consumes a lot of scratch orgs
-  I need to find a way to use a dedicated org for this test
-
-  it('should run mutation testing successfully', async () => {
-    const result = execCmd<ApexMutationTestResult>(
-      'apex mutation test run --class-file MutationClass --test-file MutationClassTest',
-      {
-        ensureExitCode: 0,
-      }
-    ).jsonOutput
-
-    expect(result).to.have.property('score', '100')
-  })
-
   it('should handle invalid class file', async () => {
     const result = execCmd<ApexMutationTestResult>(
-      'apex mutation test run --class-file InvalidClass --test-file MutationTest',
+      'apex mutation test run -o apex-mutation-testing --report-dir reports/nut --apex-class InvalidClass --test-class MutationTest',
       {
         ensureExitCode: 1,
       }
     ).shellOutput
 
-    // biome-ignore lint/suspicious/noConsoleLog: <explanation>
-    console.log(result)
-
-    expect(result).to.include('InvalidClass not found')
+    expect(result.stderr).to.include('InvalidClass not found')
   })
 
   it('should handle invalid test file', async () => {
     const result = execCmd<ApexMutationTestResult>(
-      'apex mutation test run --class-file Mutation --test-file InvalidClassTest',
+      'apex mutation test run -o apex-mutation-testing --report-dir reports/nut --apex-class Mutation --test-class InvalidClassTest',
       {
         ensureExitCode: 1,
       }
     ).shellOutput
 
-    expect(result).to.include('InvalidClassTest class not found')
+    expect(result.stderr).to.include('InvalidClassTest not found')
   })
-    */
+
+  it('should run mutation testing successfully', async () => {
+    const result = execCmd<ApexMutationTestResult>(
+      'apex mutation test run -o apex-mutation-testing --report-dir reports/nut --apex-class Mutation --test-class MutationTest --json',
+      {
+        ensureExitCode: 0,
+      }
+    ).shellOutput
+
+    const jsonResult = JSON.parse(result.stdout)
+
+    expect(jsonResult.result).to.have.property('score', 100)
+  })
 })
