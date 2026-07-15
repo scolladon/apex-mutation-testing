2da46451e40a7a8c50069c970d1e9463f243a784
Sébastien Colladon <colladonsebastien@gmail.com>
2026-07-15 16:31:46 +0200

    chore: remove obsolete re2 install-script workarounds

--- vitest.config.mutation.ts
+++ vitest.config.mutation.ts
@@ -2,7 +2,6 @@ import { defineSuite } from './vitest.config.base'
 
 export default defineSuite({
   test: {
-    setupFiles: ['./test/setup/mutation-setup.ts'],
     include: ['**/test/unit/**/*.test.ts', '**/test/integration/**/*.test.ts'],
   },
 })
